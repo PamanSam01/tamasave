c5b703acf02b0d73491a703c1ead70fd015ad23d
PamanSam01 <mrsamweb3@gmail.com>
2026-06-06 14:57:31 +0700

    fix: next 16 config support

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  transpilePackages: ['viem'],
-  webpack: (config: any) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@react-native-async-storage/async-storage": false,
-      "pino-pretty": false
-    };
-    return config;
-  }
+  transpilePackages: ['viem']
 };
 
 export default nextConfig;
